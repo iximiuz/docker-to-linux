8011379047ca29d4fa92193d57d07b57f5cedf26
Henri HANNETEL <henri.hannetel@gmail.com>
2021-07-19 22:02:40 +0200

    [create_image] write step titles in blue for easy tracking

--- create_image.sh
+++ create_image.sh
@@ -2,33 +2,41 @@
 
 set -e
 
-echo -e "[Create disk image]"
+echo_blue() {
+    local font_blue="\033[94m"
+    local font_bold="\033[1m"
+    local font_end="\033[0m"
+
+    echo -e "\n${font_blue}${font_bold}${1}${font_end}"
+}
+
+echo_blue "[Create disk image]"
 dd if=/dev/zero of=/os/${DISTR}.img bs=$(expr 1024 \* 1024 \* 1024) count=1
 
-echo -e "\n[Make partition]"
+echo_blue "[Make partition]"
 sfdisk /os/${DISTR}.img < /os/partition.txt
 
-echo -e "\n[Format partition with ext4]"
+echo_blue "\n[Format partition with ext4]"
 losetup -D
 
 LOOPDEVICE=$(losetup -f)
-echo -e "\n[Using ${LOOPDEVICE} loop device]"
+echo_blue "[Using ${LOOPDEVICE} loop device]"
 losetup -o $(expr 512 \* 2048) ${LOOPDEVICE} /os/${DISTR}.img
 mkfs.ext4 ${LOOPDEVICE}
 
-echo -e "\n[Copy ${DISTR} directory structure to partition]"
+echo_blue "[Copy ${DISTR} directory structure to partition]"
 mkdir -p /os/mnt
 mount -t auto ${LOOPDEVICE} /os/mnt/
 cp -R /os/${DISTR}.dir/. /os/mnt/
 
-echo -e "\n[Setup extlinux]"
+echo_blue "[Setup extlinux]"
 extlinux --install /os/mnt/boot/
 cp /os/${DISTR}/syslinux.cfg /os/mnt/boot/syslinux.cfg
 
-echo -e "\n[Unmount]"
+echo_blue "[Unmount]"
 umount /os/mnt
 losetup -D
 
-echo -e "\n[Write syslinux MBR]"
+echo_blue "[Write syslinux MBR]"
 dd if=/usr/lib/syslinux/mbr/mbr.bin of=/os/linux.img bs=440 count=1 conv=notrunc
 
